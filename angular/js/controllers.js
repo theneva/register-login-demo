79b8df1696f6389b404812daab2efccf1b46303b
Martin Lehmann <lehmar11@student.westerdals.no>
2015-05-04 13:57:24 +0200

    Verify password on POST /api/sessions, but no tokens yet

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -4,11 +4,35 @@ app.controller('MainController', function($scope, $http) {
 	$scope.signup = function(username, password) {
 		console.log('username', username, 'password', password);
 
+		var newUser = {
+			username: username,
+			password: password
+		};
+
+		$http.post('/api/users', newUser)
+			.success(function(user) {
+				console.log('successfully registerd', user);
+			})
+			.error(function() {
+				console.log('Something went wrong');
+			});
+	};
+
+	$scope.login = function(username, password) {
+		console.log('login', username, password);
+
 		var user = {
 			username: username,
 			password: password
 		};
 
-		$http.post('/api/users', user);
+		$http.post('/api/sessions', user)
+			.success(function() {
+				console.log('everything went well');
+			})
+			.error(function() {
+				console.log('things went badly');
+				console.log('Wrong username or password');
+			});
 	};
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcrypt');
 var mongoose = require('mongoose');
+var morgan = require('morgan');
 
 var app = express();
 var port = 5238;
@@ -22,6 +23,7 @@ app.use('/', express.static(__dirname + "/../angular"));
 
 // use body-parser for the API
 app.use('/api', bodyParser.json());
+app.use('/api', morgan('dev'));
 
 // create a user
 app.post('/api/users', function(req, res) {
@@ -29,19 +31,45 @@ app.post('/api/users', function(req, res) {
 	
 	var hash = bcrypt.hashSync(receivedUser.password, 10);
 
-	var user = new User({
-		username: receivedUser.username,
-		passwordHash: hash
-	});
+	// check if a user with that username already exists
+	User.findOne({username: receivedUser.username}, function(err, user) {
+		if (user) {
+			return res.status('401').send('Username already exists');
+		} else {
+			var user = new User({
+				username: receivedUser.username,
+				passwordHash: hash
+			});
 
-	user.save(function() {
-		res.status(201).send(user);
+			user.save(function() {
+				return res.status(201).send(user);
+			});
+		}
 	});
 });
 
 app.get('/api/users', function(req, res) {
 	User.find(function(err, users) {
 		res.send(users);
+	});
+});
+
+app.post('/api/sessions', function(req, res) {
+	var loginAttempt = req.body;
+
+	User.findOne({username: loginAttempt.username}, function(err, user) {
+		if (!user) {
+			return res.status(401).send('Wrong username');
+		}
+
+		var passwordsMatch = bcrypt.compareSync(loginAttempt.password, user.passwordHash);
+
+		if (passwordsMatch) {
+			// user was logged in
+			return res.status(200).send('ok');
+		} else {
+			return res.status(401).send('Wrong password');
+		}
 	});
 });
 
